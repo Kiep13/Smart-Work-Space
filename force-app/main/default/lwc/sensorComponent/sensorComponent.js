3939d9996edba9a1fdf623ee31cc4a9ef966e9e2
nina_smirnova <kiep13@bk.ru>
2020-09-03 14:25:05 +0300

    added security control to sensor controller, test for this cases, and imrove error handling in sensor component

--- force-app/main/default/lwc/sensorComponent/sensorComponent.js
+++ force-app/main/default/lwc/sensorComponent/sensorComponent.js
@@ -2,11 +2,15 @@ import {LightningElement,wire, track} from 'lwc';
 import getSensors from '@salesforce/apex/SensorComponentController.getSensors';
 import getEvents from '@salesforce/apex/SensorComponentController.getEvents';
 
+const EXCEPTION_NO_ACCESS = 'System.NoAccessException';
+const MESSAGE_NO_ACCESS = 'You don\'t have access to this data';
+
 export default class SensorComponent extends LightningElement {
 
-  @wire(getSensors, {}) sensors;
+  @track sensors;
   @track events;
-  @track error;
+  @track sensorsError;
+  @track eventsError;
 
   @track eventsSize;
 
@@ -17,7 +21,20 @@ export default class SensorComponent extends LightningElement {
         this.eventsSize = result.length > 0;
     })
     .catch(error => {
-      this.error = error;
+      if(EXCEPTION_NO_ACCESS.localeCompare(error.body.exceptionType) == 0) {
+        this.eventsError = MESSAGE_NO_ACCESS;
+      }
     });
+  }
+
+  @wire(getSensors, {})
+  getSensors({error, data}) {
+    if(error) {
+      if(EXCEPTION_NO_ACCESS.localeCompare(error.body.exceptionType) == 0) {
+          this.sensorsError = MESSAGE_NO_ACCESS;
+      }
+    } else if(data) {
+      this.sensors = data;
+    }
   }
 }
